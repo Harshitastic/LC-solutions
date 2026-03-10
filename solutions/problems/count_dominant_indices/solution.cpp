38cdcae7cbb8bd6df4730b477ddd1ba32bdf4d76
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-03-10 11:24:39 +0000

    Sync LeetCode submission - Count Dominant Indices (cpp)

--- solutions/problems/count_dominant_indices/solution.cpp
+++ solutions/problems/count_dominant_indices/solution.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int dominantIndices(vector<int>& nums) {
         int n=nums.size();
-        vector<int> pre(n);
-        pre[0]=nums[0];
-        for(int i=1;i<n;i++) pre[i]=pre[i-1]+nums[i];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+
+        }
+        int dem=n;
         int cnt=0;
-        for(int i=0;i<n-1;i++){
-            int sum=(pre[n-1]-pre[i]);
-            if(nums[i]*(n-i-1)>sum) cnt++;
+        for(int i=0;i<=n-2;i++){
+            sum-=nums[i];
+            dem--;
+            if(nums[i]>sum/dem) cnt++;
+            
         }
         return cnt;
+        
     }
 };
