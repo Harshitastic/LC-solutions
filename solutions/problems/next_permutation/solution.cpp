4226a80d99138bdaeea643c41820c291c928538c
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-03-23 06:02:05 +0000

    Sync LeetCode submission - Next Permutation (cpp)

---
+++ solutions/problems/next_permutation/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int idx=-1;
+        int n=nums.size();
+        int i;
+        for(i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(i==-1){
+            reverse(nums.begin(),nums.end());
+            
+
+        }
+        else{
+        for(int i =n-1;i>idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[idx],nums[i]);
+                break;
+            }
+        }
+        reverse(nums.begin()+idx+1,nums.end());
+        }
+        
+    }
+};
