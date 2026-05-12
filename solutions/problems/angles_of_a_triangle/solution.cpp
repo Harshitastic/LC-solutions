84771631f25a11882bdb3aa7ce247103463c5b54
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-05-12 08:59:49 +0000

    Sync LeetCode submission - Angles of a Triangle (cpp)

---
+++ solutions/problems/angles_of_a_triangle/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<double> internalAngles(vector<int>& sides) {
+        vector<int> s =sides;
+        sort(s.begin(),s.end());
+        double a = s[0], b = s[1], c=s[2];
+
+        if (a+b<=c) return{};
+        double A = acos((b*b +c*c - a*a)/(2*b*c)) * 180.0/M_PI;
+        double B = acos((a*a +c*c - b*b) /(2*a*c)) * 180.0/M_PI;
+
+        return {A, B, 180.0 - A-B};
+        
+        
+    }
+};
