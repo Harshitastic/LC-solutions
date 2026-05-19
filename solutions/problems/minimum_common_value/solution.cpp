a6a3f884735dab921197401f4558bc5c77f4e125
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-05-19 07:11:33 +0000

    Sync LeetCode submission - Minimum Common Value (cpp)

---
+++ solutions/problems/minimum_common_value/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        int i=0,j=0;
+        int ans=-1;
+        while(i<n && j<m){
+            if(nums1[i]==nums2[j]){
+                ans=nums1[i];
+                return ans;
+            }
+            else if(nums1[i]>nums2[j]) j++;
+            else  i++;
+
+        }
+        return ans;
+    }
+};
