b3ed15107ad4a59453f4124381fbc170d00a77f1
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-05-29 14:11:39 +0530

    Time: 0 ms (100%), Space: 9.2 MB (20.49%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
+    int helper(const string &s,int i,long long num,int sign){
+        int n=s.size();
+        if(i>=n || !isdigit(s[i])) return (int)(sign*num);
+        num=num*10+(s[i]-'0');
+
+        if(sign*num <=INT_MIN) return INT_MIN;
+        else if (sign*num>=INT_MAX) return INT_MAX;
+        return helper(s,i+1,num,sign);
+    }
     int myAtoi(string s) {
-        int n=s.length();
-        long long ans=0;
         int i=0;
-        bool neg=false;
+        int n=s.size();
         while(i<n && s[i]==' ') i++;
-        if(i<n && (s[i]=='-' || s[i]=='+')){
-            if(s[i]=='-') neg=true;
+        int sign=1;
+        if(i<n && s[i]=='+' || s[i]=='-'){
+            if(s[i]=='-') sign=-1;
             i++;
         }
-        while(i<n){
-            if(s[i]<'0' || s[i]>'9') break;
-            ans=ans*10+(s[i]-'0');
-            if(neg){
-                if(-ans<INT_MIN) return INT_MIN;
-            }
-            else{
-                if(ans>INT_MAX) return INT_MAX;
-            }
-            i++;
-        }
-        if(neg) return (int)-ans;
-        return (int)ans;
+        return helper(s,i,0,sign);
     }
 };
