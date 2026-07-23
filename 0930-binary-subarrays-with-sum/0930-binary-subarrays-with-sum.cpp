ddaca04238770c11a735ea775cff32ed8815fcdf
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-07-23 22:31:49 +0530

    Time: 2 ms (57.61%), Space: 32.7 MB (39.58%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int>psum;
-        int cnt=0,sum=0;
-        psum[0]=1;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(psum.find(sum-goal)!=psum.end()) cnt+=psum[sum-goal];
-            psum[sum]++;
-
+    int func(vector<int>& nums,int goal){
+        int l=0,r=0,sum=0,cnt=0;
+        if(goal<0) return 0;
+        while(r<nums.size()){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
+            }
+            cnt+=(r-l+1);
+            r++;
 
         }
         return cnt;
-        
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums,goal)-func(nums,goal-1);
     }
 };
