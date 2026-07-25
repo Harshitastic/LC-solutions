c6d61ea32149a7a3a35c14627cff1f1d8c85012b
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-07-25 18:44:35 +0530

    Time: 4 ms (4.58%), Space: 10 MB (5.88%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        string s=to_string(n);
+        sort(s.rbegin(),s.rend());
+        int ans = (s[0]-'0') *(s[1]-'0');
+        return ans;
+    }
+};
