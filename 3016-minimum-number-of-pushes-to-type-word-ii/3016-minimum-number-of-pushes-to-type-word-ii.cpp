79adb7c2b62b002d69cf5018179abc42434c0604
Harshit Jhariya <jhariyaharshit1@gmail.com>
2026-07-31 10:07:02 +0530

    Time: 8 ms (61.48%), Space: 28.5 MB (25.39%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    int minimumPushes(string word) {
-        int n=word.length();
-        vector<int>freq(26,0);
-        for(char c:word){
-            freq[c-'a']++;
+class Solution
+{
+    public:
+        int minimumPushes(string word)
+        {
+            int n = word.length();
+            vector<int> freq(26, 0);
+            for (char c: word)
+            {
+                freq[c - 'a']++;
+            }
+            int ans = 0;
+            sort(freq.rbegin(), freq.rend());
+            int cost = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                if (!freq[i]) break;
+                cost = ceil((i + 1) / 8.0);
+                ans += cost *freq[i];
+            }
+            return ans;
         }
-        int ans=0;
-        sort(freq.rbegin(),freq.rend());
-        int cost=0;
-        for(int i=0;i<26;i++){
-            if(!freq[i]) break;
-            cost=(i/8)+1;
-            ans+=cost*freq[i];
-            
-        }
-        return ans;
-
-    }
 };
